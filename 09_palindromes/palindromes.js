787edaee322b5e5dcee61a1f727aad56629f56be
Yethu <yethukichu55@gmail.com>
2022-06-14 20:39:41 +0530

    js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function(string) {
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
+  };
+  
+  module.exports = palindromes;
